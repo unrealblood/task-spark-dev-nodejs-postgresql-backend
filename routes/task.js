0ef27b5ec2ad9b4a7222a35193e923f058c41705
Shubham Singh <shubham110084@outlook.com>
2026-03-05 12:51:34 +0530

    add more tasks routes

--- routes/task.js
+++ routes/task.js
@@ -40,3 +40,62 @@ tasksRouter.post("/add-task", async (req, res) => {
         return res.json({success: false, error: "Failed to add task in database. Error: " + error.message});
     }
 });
+
+tasksRouter.put("/update-task-content/:taskId", async (req, res) => {
+    const { taskId } = req.params;
+    const { newContent } = req.body;
+
+    try {
+        await pool.query("CREATE TABLE IF NOT EXISTS tasks (_id SERIAL PRIMARY KEY, content VARCHAR(200) NOT NULL, completed BOOL NOT NULL, userId INT NOT NULL)");
+
+        const result = await pool.query("UPDATE tasks SET content = $1 WHERE _id = $2", [newContent, taskId]);
+
+        if(result.rowCount > 0) {
+            return res.json({success: true});
+        }
+
+        return res.json({success: false});
+    }
+    catch(ex) {
+        return res.json({success: false, error: "Failed to update task in database. Error: " + ex.message});
+    }
+});
+
+tasksRouter.put("/update-task-completed/:taskId", async (req, res) => {
+    const { taskId } = req.params;
+    const { newCompleted } = req.body;
+
+    try {
+        await pool.query("CREATE TABLE IF NOT EXISTS tasks (_id SERIAL PRIMARY KEY, content VARCHAR(200) NOT NULL, completed BOOL NOT NULL, userId INT NOT NULL)");
+
+        const result = await pool.query("UPDATE tasks SET completed = $1 WHERE _id = $2", [newCompleted, taskId]);
+
+        if(result.rowCount > 0) {
+            return res.json({success: true});
+        }
+
+        return res.json({success: false});
+    }
+    catch(ex) {
+        return res.json({success: false, error: "Failed to update task in database. Error: " + ex.message});
+    }
+});
+
+tasksRouter.delete("/delete-task/:taskId", async (req, res) => {
+    const { taskId } = req.params;
+
+    try {
+        await pool.query("CREATE TABLE IF NOT EXISTS tasks (_id SERIAL PRIMARY KEY, content VARCHAR(200) NOT NULL, completed BOOL NOT NULL, userId INT NOT NULL)");
+
+        const result = await pool.query("DELETE FROM tasks WHERE _id = $1", [taskId]);
+
+        if(result.rowCount > 0) {
+            return res.json({success: true});
+        }
+
+        return res.json({success: false});
+    }
+    catch(ex) {
+        return res.json({success: false, error: "Failed to delete task in database. Error: " + ex.message});
+    }
+});
